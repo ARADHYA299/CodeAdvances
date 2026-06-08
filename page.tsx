0806346ba5168a3194c29d545936986d70101539
Aradhya Nautiyal <134077675+ARADHYA299@users.noreply.github.com>
2026-06-08 23:39:34 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+  return(
+    <main className="flex items-center justify-between
+mb-4">
+      <h1 className="p-6 text-xl align-center">TheBoardRoom </h1>
+    </main>
+  )
+}
