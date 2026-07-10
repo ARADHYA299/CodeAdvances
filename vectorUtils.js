0d76c23d8a210135fb7220173a5e9b993bcdabf1
Aradhya Nautiyal <134077675+ARADHYA299@users.noreply.github.com>
2026-07-10 23:57:41 +0530

    Add files via upload

---
+++ vectorUtils.js
@@ -0,0 +1,22 @@
+exports.cosineSimilarity = (a, b) => {
+  if (a.length !== b.length) {
+    throw new Error("Embedding dimensions do not match");
+  }
+
+  let dot = 0;
+  let magA = 0;
+  let magB = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    dot += a[i] * b[i];
+    magA += a[i] * a[i];
+    magB += b[i] * b[i];
+  }
+
+  magA = Math.sqrt(magA);
+  magB = Math.sqrt(magB);
+
+  if (magA === 0 || magB === 0) return 0;
+
+  return dot / (magA * magB);
+};
